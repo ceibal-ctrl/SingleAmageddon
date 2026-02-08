79d67ef7935bf2a5c9bf648138ffe0d1bf86d2bc
angeldross <angeldross55@gmail.com>
2026-02-08 20:12:46 +0300

    Add age calculator for year 2050

--- WHATEVER.py
+++ WHATEVER.py
@@ -7,3 +7,9 @@ def calculate_age(birth_year):
 birth_year = int(input("Enter your birth year: "))
 age = calculate_age(birth_year)
 print(f"You are {age} years old.")
+
+def calculate_age_2050(birth_year):
+    target_year = 2050
+    return target_year - birth_year
+age_2050 = calculate_age_2050(birth_year)
+print(f"In the year 2050, you will be {age_2050} years old.")
